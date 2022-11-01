13f6cebc4df305084e5cdf96435419cb513cfff0
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-11-01 15:08:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import Navbar from "./page/navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Register from "./page/Register/Register";
 import Sidebar from "./page/sidebar/Sidebar";
-import { Login } from "./page/home/login/Login";
-
+import {Login} from "./page/home/login/Login";
 
 function App() {
   return (
     <div>
       <Routes>
-         
-          <Route path="/register" element={<Register/>}/>
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navbar />}>
           <Route path="/home" element={<Sidebar />} />
         </Route>
-
       </Routes>
     </div>
   );
