4454477716811abc24dc9273a65b9a3524c9f094
SonTuyen123 <105645969+SonTuyen123@users.noreply.github.com>
2022-11-01 15:39:11 +0700

    Dev tuyen (#9)
    * update
    
    * update
    
    * add
    
    * add
    
    * add
    
    * add
    
    Co-authored-by: Admin <hanhngao@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./page/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
+
 import Register from "./page/Register/Register";
 import Sidebar from "./page/sidebar/Sidebar";
 import { Login } from "./page/home/login/Login";
@@ -9,13 +10,11 @@ function App() {
   return (
     <div>
       <Routes>
-         
           <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navbar />}>
           <Route path="/home" element={<Sidebar />} />
         </Route>
-
       </Routes>
     </div>
   );
