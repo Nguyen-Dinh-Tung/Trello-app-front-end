8cbb45b2a81b9c7e903e447a3307c45f4d3ab86e
Admin <hanhngao@gmail.com>
2022-11-01 11:38:55 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import Navbar from './page/navbar/Navbar';
-
+import Navbar from "./page/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./page/home/Home";
-import Sidebar from './page/sidebar/Sidebar';
+import Sidebar from "./page/sidebar/Sidebar";
 
 function App() {
   return (
     <div>
-      <Sidebar/>
-      <Navbar/>
+      <Sidebar />
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
