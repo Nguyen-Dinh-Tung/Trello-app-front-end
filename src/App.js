71198022228a586c8328ffd02ba8104d34cfda3e
Vũ Minh Anh <vuminhanh@MacBook-Pro-cua-Vu-2.local>
2022-11-01 14:45:03 +0700

    register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./page/home/Home";
+import Register from "./page/Register/Register";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+
       </Routes>
     </div>
   );
