535b3bf376b74711d3a0cc8ea1a47bb41675275e
Admin <hanhngao@gmail.com>
2022-11-01 16:59:26 +0700

    add

--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -12,7 +12,7 @@ const registerSchema = Yup.object().shape({
   password: Yup.string().required("Required"),
   confirmpassword: Yup.string()
     .oneOf([Yup.ref("password")], "Password done not match !")
-    .required("Required !"),
+    .required("Required"),
 });
 
 export default function Register() {
@@ -110,7 +110,6 @@ export default function Register() {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth={2}
-
                             d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                           />
                         </svg>
@@ -130,7 +129,6 @@ export default function Register() {
                             d="M6 18L18 6M6 6l12 12"
                           />
                         </svg>
-
                       </button>
                     </div>
                   ) : null}
