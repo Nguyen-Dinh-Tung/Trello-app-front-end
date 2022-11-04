df3d740d702d710b94dad1857727805b81d9f38a
Thang cg <duongsag.hp11@gmail.com>
2022-11-04 13:28:46 +0700

    update file isLogin,navbar,sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -185,32 +185,6 @@ export default function Navbar() {
           <IsLogin/>
         </div>
       </nav>
-      <ul id="dropdown-user" className="fixed mt-2 w-60 bg-gray-50  rounded-lg" style={{marginLeft:1290+'px',color:'#e34687'}}>
-                  <li className="flex">
-                    <a
-                      href="#"
-                      className="flex w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11 "
-                    >
-                      <span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-4 h-4 mt-1"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-                          />
-                        </svg>
-                      </span>
-                      <span className="ml-2">Bảng</span>
-                    </a>
-                  </li>
-                </ul>
       <Outlet></Outlet>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import axios from "axios";
 import store from "../src/redux/store";
 import {Provider} from "react-redux";
+import "./index.css"
 ReactDOM.render(
   <React.StrictMode>
     <Router>
