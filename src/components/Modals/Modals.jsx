14f22183407d70e2088864aca9b0e94e09d609cc
Thang cg <duongsag.hp11@gmail.com>
2022-11-08 11:08:24 +0700

    pull dev ngay 11/8

--- src/components/Modals/Modals.jsx
+++ src/components/Modals/Modals.jsx
@@ -49,7 +49,7 @@ function Modals(props) {
             className="container mx-auto w-11/12 md:w-2/3 max-w-lg"
           >
             <div className="relative py-8 px-5 md:px-10 bg-white dark:bg-gray-800 dark:border-gray-700 shadow-md rounded border border-gray-400">
-              <h1 className="text-gray-800 dark:text-white  font-lg font-bold tracking-normal leading-tight mb-4">
+              <h1 className="text-gray-800   font-lg font-bold tracking-normal leading-tight mb-4">
                 Thêm bảng mới
               </h1>
               <label
