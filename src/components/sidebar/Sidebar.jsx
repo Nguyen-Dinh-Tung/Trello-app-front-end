451f19879690731cc3037dcb344592a3b9732a38
SonTuyen123 <105645969+SonTuyen123@users.noreply.github.com>
2022-11-14 09:15:00 +0700

    Modal (#51)
    * update drop drag
    
    * update drop drag
    
    * update broad
    
    * update
    
    * update
    
    * update drop drag
    
    * update broad
    
    * update drop drag
    
    * update
    
    * update
    
    * update
    
    * add
    
    * update
    
    * update
    
    * adada
    
    * register
    
    * add
    
    * up avata and resetpass
    
    * up avata and resetpass new
    
    * tuyenupdate 9:48
    
    * add
    
    * addadada
    
    * adada
    
    * adad
    
    * bigupdate
    
    * update- 3:19
    
    * adadada
    
    * dadđ
    
    * adadad
    
    * adada
    
    * update-img-share
    
    * adad
    
    * adada
    
    * adadad
    
    * dadada
    
    * adadada
    
    * upload-icon-Loading-accoutn
    
    * dadad
    
    * adadad
    
    * adadad
    
    * adada
    
    * adadad
    
    * adada
    
    * adadad
    
    * adadad
    
    Co-authored-by: Nguyen-Dinh-Tung <s30.tung@gmail.com>
    Co-authored-by: Admin <hanhngao@gmail.com>

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -70,40 +70,17 @@ export default function Sidebar(props) {
                 </Button>
                 <span className="my-auto text-3xl font-bold">{item.name}</span>
                 <div className=" ml-40  flex flex-row gap-3 mb-6">
-                  <div className="flex hover:bg-slate-300 bg-slate-200 p-1 px-2 rounded cursor-pointer">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 022 20"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-4 h-4 mt-0.5 items-center mr-0.5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-                      />
-                    </svg>
-                    <a>Bảng</a>
+                  <div className="flex hover:bg-slate-300 bg-slate-200 mt-2 p-1 px-2 rounded cursor-pointer">
+                    <span className="block mr-1">
+                      <i class="fa-brands fa-trello"></i>
+                    </span>
+                    <span> Bảng </span>
                   </div>
-                  <div className="flex hover:bg-slate-300 bg-slate-200 p-1 px-2 rounded cursor-pointer">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-4 h-4 mt-1 mr-3"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                      />
-                    </svg>
-
-                    <a>Thành viên()</a>
+                  <div className="flex hover:bg-slate-300 bg-slate-200 mt-2 p-1 px-2 rounded cursor-pointer">
+                    <span className="block mr-1">
+                      <i class="fa-solid fa-user-group"></i>
+                    </span>
+                    <span> Thành viên </span>
                   </div>
                 </div>
               </div>
