5d0a0e93ad08bffa29a2b71e427bc38d0c333b5f
Nguyen-Dinh-Tung <99470919+Nguyen-Dinh-Tung@users.noreply.github.com>
2022-11-15 14:38:59 +0700

    Commenta (#63)
    * update comment avatar
    
    * update comment avatar and name

--- src/components/Comment/CommentItem.jsx
+++ src/components/Comment/CommentItem.jsx
@@ -11,22 +11,65 @@ import { useEffect } from 'react';
 import getImage from '../../api/getImage';
 import { useState } from 'react';
 export default function CommentItem(props) {
-  let token = localStorage.getItem('token') ;
-  let idUser = jwtDecode(token).id 
   const data = props.data ;
-  const [urlAvatar , setUrlAvatar] = useState()
+  const idUser = props.data.idUser;
+  const [urlAvatar , setUrlAvatar] = useState();
+  const [name , setName] = useState('');
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+        fontSize: 30,
+      },
+      children: `${name.split(" ")[0][0]}`,
+    };
+  }
+
+
   useEffect(() =>{
     getImage(idUser)
-    .then(res => setUrlAvatar(res.data.message))
+    .then(res => {
+      console.log(res.data);
+        setName(res.data.name)
+      if(res.data.message != ''){
+        setUrlAvatar(res.data.message)
+      }
+    })
     .catch(e => console.log(e.message))
-  } , [] )
+  } , [] );
+
+
   return (
     <ListItem alignItems="flex-start">
       <ListItemAvatar>
-        <Avatar alt="Remy Sharp" src={urlAvatar && urlAvatar} />
+        {urlAvatar != 'Không có ảnh!'? 
+        <Avatar  src={urlAvatar} />
+        :
+        <Avatar {...stringAvatar(name)}/>
+        }
       </ListItemAvatar>
       <ListItemText
-        primary={data.email}
+        primary={name && name}
         secondary={
           <React.Fragment>
             <Typography
